4eaa49fbbd1cd46e78f9da85ad22e1c492b45d31
SusieHatter <susiivaldez@gmail.com>
2022-01-03 23:22:45 -0600

    add auto post functionality

--- src/config.js
+++ src/config.js
@@ -4,4 +4,6 @@ module.exports = {
   token: process.env["DISCORD_TOKEN"],
   guildId: process.env["DISCORD_GUILD_ID"],
   clientId: process.env["DISCORD_CLIENT_ID"],
+  subreddits: process.env["AUTO_POST_SUBREDDIT_LIST"].split(","),
+  autoPostChannelId: process.env["AUTO_POST_CHANNEL_ID"],
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const { Client, Intents } = require("discord.js");
-const { token } = require("./config.js");
+const { token, subreddits, autoPostChannelId } = require("./config.js");
 const { getPost } = require("./reddit.js");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -23,5 +23,31 @@ async function handlePost(interaction) {
   const post = await getPost(subreddit);
   interaction.reply(post.url);
 }
+
+async function postRandomPost(subreddit) {
+  const channel = client.channels.cache.get(autoPostChannelId);
+  const post = await getPost(subreddit);
+  channel.send(post.url);
+}
+
+async function postRandomPosts() {
+  subreddits.forEach((subreddit) => {
+    postRandomPost(subreddit);
+  });
+}
+
+const POST_TIME_CHECK_INTERVAL = 1000 * 60 * 60;
+
+function isPostingTime(date = new Date()) {
+  const hour = date.getHours();
+  return hour === 7;
+}
+
+setInterval(() => {
+  if (!isPostingTime()) {
+    return;
+  }
+  postRandomPosts();
+}, POST_TIME_CHECK_INTERVAL);
 
 client.login(token);
